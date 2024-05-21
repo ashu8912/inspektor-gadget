ashu8912/inspektor-gadget

commit 740976d3ab0fb8e9d046f1ca69f812a43aed01e4

pkg/operators: Enable tracing hosts events for attacher gadgets.
Suggested-by: Mauricio Vásquez <mauriciov@microsoft.com>
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/operators/localmanager/localmanager.go
@@ -314,6 +314,12 @@ func (l *localManagerTrace) PreGadgetRun() error {
 			},
 		)
 
+		if host {
+			// We need to attach this fake container for gadget which rely only on the
+			// Attacher concept.
+			containers = append(containers, &containercollection.Container{Pid: 1})
+		}
+
 		for _, container := range containers {
 			attachContainerFunc(container)
 		}
@@ -328,12 +334,20 @@ func (l *localManagerTrace) PostGadgetRun() error {
 		l.manager.igManager.RemoveMountNsMap(l.subscriptionKey)
 	}
 	if l.subscriptionKey != "" {
+		host := l.params.Get(Host).AsBool()
+
 		log.Debugf("calling Unsubscribe()")
 		l.manager.igManager.Unsubscribe(l.subscriptionKey)
 
 		// emit detach for all remaining containers
 		for container := range l.attachedContainers {
 			l.attacher.DetachContainer(container)
+		}
+
+		if host {
+			// Reciprocal operation of attaching fake container with PID 1 which is
+			// needed by gadgets relying on the Attacher concept.
+			l.attacher.DetachContainer(&containercollection.Container{Pid: 1})
 		}
 	}
 	return nil

commit 1581bace540903503e88fae4f332e4819713cc35

pkg/operators: Fix tracing host events for mount namespace ID attacher gadgets.
Some attacher-based gadgets, like traceloop and advise seccomp-profile, need
the mount namespace ID of the container to bet set.
With --host, we only created a fake container structure containing only PID 1.
So, traceloop and advise seccomp-profile would not work correctly as their
required information were not set.

To cope with this, we extend the fake container structure to also contain the
mount namespace ID of PID 1.

Fixes: 740976d3ab0f ("pkg/operators: Enable tracing hosts events for attacher gadgets.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/operators/localmanager/localmanager.go
@@ -370,9 +370,24 @@ func (l *localManagerTrace) PreGadgetRun() error {
 		}
 
 		if host {
+			pidOne := uint32(1)
+			mntns, err := containerutils.GetMntNs(int(pidOne))
+			if err != nil {
+				return fmt.Errorf("getting mount namespace ID for host PID %d: %w", pidOne, err)
+			}
+
 			// We need to attach this fake container for gadget which rely only on the
-			// Attacher concept.
-			containers = append(containers, &containercollection.Container{Pid: 1})
+			// Attacher concept:
+			// * Network gadget will get the netns corresponding to PID 1 on their
+			//   own.
+			// * Others, like traceloop or advise seccomp-profile, need the mount
+			//   namespace ID to bet set.
+			fakeContainer := &containercollection.Container{
+				Pid:   pidOne,
+				Mntns: mntns,
+			}
+
+			containers = append(containers, fakeContainer)
 		}
 
 		for _, container := range containers {
@@ -401,9 +416,20 @@ func (l *localManagerTrace) PostGadgetRun() error {
 			}
 
 			if host {
+				pidOne := uint32(1)
+				mntns, err := containerutils.GetMntNs(int(pidOne))
+				if err != nil {
+					return fmt.Errorf("getting mount namespace ID for host PID %d: %w", pidOne, err)
+				}
+
+				fakeContainer := &containercollection.Container{
+					Pid:   pidOne,
+					Mntns: mntns,
+				}
+
 				// Reciprocal operation of attaching fake container with PID 1 which is
 				// needed by gadgets relying on the Attacher concept.
-				l.attacher.DetachContainer(&containercollection.Container{Pid: 1})
+				l.attacher.DetachContainer(fakeContainer)
 			}
 		}
 	}